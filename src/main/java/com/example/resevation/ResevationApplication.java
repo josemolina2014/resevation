cda4b29dd8f0ae318dc93f82d5c4b7225b9eea85
jose molina <josmoli@gmail.com>
2020-03-01 22:03:30 -0500

    initial commit

---
+++ src/main/java/com/example/resevation/ResevationApplication.java
@@ -0,0 +1,13 @@
+package com.example.resevation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResevationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResevationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/resevation/ResevationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resevation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResevationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
